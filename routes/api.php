9550c1d9ce717c78d0ffe1f1cd57a0de34cef154
Marcus Reis <mvsantosreis@gmail.com>
2024-05-05 12:53:56 +0100

    Sanctum Security Implementation

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UrlShortenerController;
 
 /*
@@ -19,6 +20,19 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
 
-Route::post('/shortener', [UrlShortenerController::class, 'index']);
-Route::get('/shortener-list', [UrlShortenerController::class, 'list']);
+// Protected routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::post('/shortener', [UrlShortenerController::class, 'index']);
+    Route::get('/shortener-list', [UrlShortenerController::class, 'list']);
+});
+
+
+
+
+
+
